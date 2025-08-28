f636158daf323be09bc700d5fc2214727c678358
Seth <sstiles2@huskers.unl.edu>
2025-08-27 19:13:45 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Seth  Stiles
+ * Date: 6/27/2025
+ *
+ *
+ *Program to print my name
+ *
+ */
+
+ #include <stdio.h>
+
+
+
+ int main(int argc, char const *argv[]) {
+    printf("Seth Stiles\n");
+
+ }
